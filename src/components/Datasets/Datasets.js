ceb76b586422669afb069f88d2fc0431fce71018
elohhim <elohhim@gmail.com>
2019-05-19 21:03:39 +0200

    Dataset creation and deletion WIP

--- src/components/Datasets/Datasets.js
+++ src/components/Datasets/Datasets.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Datasets.css';
 import 'katex/dist/katex.min.css';
-import {Button, Form, Table} from "react-bootstrap";
+import {Button, Form, Table, Modal} from "react-bootstrap";
 import {NavTab, RoutedTabs} from "react-router-tabs";
 import {Redirect, Route, Switch} from "react-router-dom";
 import {BlockMath} from "react-katex";
 import Content from "../Content/Content";
 import {ErrorMessage, Field, Form as FormikForm, Formik} from "formik";
 import * as Yup from "yup";
+
+// TODO: 18.05.2019 John - create proper service layer
+const datasetsEndpoint = "http://localhost:5000/api/datasets/";
 
 function Datasets({match}) {
   return (
@@ -34,13 +37,26 @@ class DatasetList extends React.Component {
     this.state = {
       datasets: []
     };
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
     // TODO: 12.05.2019 John - proper service layer and some state management - redux?
-    fetch("http://localhost:5000/datasets/api/list")
+    fetch(datasetsEndpoint)
       .then(res => res.json())
       .then(json => this.setState({datasets: json}));
+  }
+
+  handleDelete(datasetId) {
+    fetch(datasetsEndpoint + `${datasetId}`, {
+      method: 'DELETE'
+    }).then(res => {
+      if (res.status === 204) {
+        this.setState({
+          datasets: this.state.datasets.filter((d) => d.id !== datasetId)
+        });
+      }
+    });
   }
 
   render() {
@@ -53,17 +69,18 @@ class DatasetList extends React.Component {
           <th scope="col">Name</th>
           <th scope="col">Size</th>
           <th scope="col">Function</th>
+          <th scope="col">Actions</th>
         </tr>
         </thead>
         <tbody>
-        {this.state.datasets.map(d => <DatasetRow key={d.id} dataset={d}/>)}
+        {this.state.datasets.map(d => <DatasetRow key={d.id} dataset={d} handleDelete={this.handleDelete}/>)}
         </tbody>
       </Table>
     );
   }
 }
 
-function DatasetRow({dataset}) {
+function DatasetRow({dataset, handleDelete}) {
   return (
     <tr>
       <th scope="row">
@@ -77,6 +94,11 @@ function DatasetRow({dataset}) {
       </td>
       <td>
         <BlockMath>{`\\begin{gathered} ${dataset.source_function} \\end{gathered}`}</BlockMath>
+      </td>
+      <td>
+        <Button variant="danger" size="sm" onClick={() => handleDelete(dataset.id)}>
+          Delete
+        </Button>
       </td>
     </tr>
   );
@@ -96,36 +118,63 @@ const DatasetCreateSchema = Yup.object().shape({
 function DatasetCreate() {
   return (
     <>
-      <h3>Create</h3>
       <Formik
-        initialValues={{size: 10000}}
+        initialValues={{size: 10000, name: ""}}
         validationSchema={DatasetCreateSchema}
         onSubmit={(values, actions) => {
-          console.log(values);
+          fetch(datasetsEndpoint, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values),
+          });
+          setTimeout(() => actions.setSubmitting(false), 500);
         }}
-        render={({errors}) => (
-          <Form noValidate as={FormikForm}>
+        render={({errors, isSubmitting}) => (
+          <Form as={FormikForm} noValidate>
             <Form.Row>
               <Form.Group controlId="name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control as={Field} name="name" isInvalid={errors.name}/>
+                <Form.Control
+                  as={Field}
+                  type="text"
+                  name="name"
+                  placeholder="Generated"
+                  isInvalid={!!errors.name}
+                />
                 <Form.Control.Feedback type="invalid">
                   <ErrorMessage name="name"/>
                 </Form.Control.Feedback>
+                <Form.Text>If left empty name will be generated as readable and easy memorizable 3 word (e.g.
+                  purely-full-bison)</Form.Text>
               </Form.Group>
             </Form.Row>
             <Form.Row>
               <Form.Group controlId="size">
                 <Form.Label>Size</Form.Label>
-                <Form.Control as={Field} type="number" name="size" step="100" isInvalid={errors.size}/>
+                <Form.Control
+                  as={Field}
+                  type="number"
+                  name="size"
+                  step="100"
+                  isInvalid={!!errors.size}
+                />
                 <Form.Control.Feedback type="invalid">
                   <ErrorMessage name="size"/>
                 </Form.Control.Feedback>
               </Form.Group>
             </Form.Row>
-            <p>
-              <Button type="submit" variant="success">Create</Button>
-            </p>
+            <Form.Row>
+              <Button
+                type="submit"
+                variant="success"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Submitting..." : "Create"}
+              </Button>
+            </Form.Row>
           </Form>
         )}
       />
